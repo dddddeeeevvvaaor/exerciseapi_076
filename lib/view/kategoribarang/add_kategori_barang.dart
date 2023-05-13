08de4c0ced21dce8c6c73b564cd0c6109ed39437
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-05-13 19:08:18 +0700

    merubah sintak class Add_Kategori_Barang

--- lib/view/kategoribarang/add_kategori_barang.dart
+++ lib/view/kategoribarang/add_kategori_barang.dart
@@ -1,66 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:materi_flutterapi/controller/kategori_barang_controller.dart';
 import 'package:materi_flutterapi/model/kategori_barang_model.dart';
+import 'package:materi_flutterapi/view/kategoribarang/kategori_barang.dart';
 
 class Add_Kategori_Barang extends StatefulWidget {
-  const Add_Kategori_Barang({super.key});
+    final Kategori_Barang_Model kategori_barang_model;
+  final Function saveChanges;
+
+  const Add_Kategori_Barang({
+    Key? key,
+    required this.kategori_barang_model,
+    required this.saveChanges,
+  }) : super(key: key);
 
   @override
   State<Add_Kategori_Barang> createState() => _Add_Kategori_BarangState();
 }
 
 class _Add_Kategori_BarangState extends State<Add_Kategori_Barang> {
-  final kategori_barang_controller = Kategori_Barang_Controller();
-  String? nama;
+  late Kategori_Barang_Controller _kategori_barang_controller;
+  Future<List<Kategori_Barang_Model>>? _kategori_barang_model;
 
-  void addKategoriBarang() async {
-    final kategori_barang = await kategori_barang_controller
-        .addKategoriBarang(Kategori_Barang_Model(nama: nama!));
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _kategori_barang_controller = Kategori_Barang_Controller();
+    _kategori_barang_model = _kategori_barang_controller.getKategoriBarang();
   }
 
   @override
   Widget build(BuildContext context) {
-    var formkey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tambah Kategori Barang'),
+        title: const Text('Add Kategori Barang'),
       ),
       body: Form(
-        key: formkey,
+        key: _formKey,
         child: ListView(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           children: [
             TextFormField(
+              controller: TextEditingController(
+                  text: widget.kategori_barang_model.nama),
               decoration: const InputDecoration(
-                hintText: 'Nama Kategori Barang',
                 labelText: 'Nama Kategori Barang',
+                border: OutlineInputBorder(),
               ),
-              onChanged: (value) {
-                nama = value;
-              },
               validator: (value) {
                 if (value == null || value.isEmpty) {
-                  return 'Nama Kategori is required';
+                  return 'Nama Kategori Barang tidak boleh kosong';
                 }
                 return null;
               },
+              onSaved: (value) {
+                widget.kategori_barang_model.nama = value!;
+              },
             ),
-            const SizedBox(height: 16.0),
+            const SizedBox(
+              height: 16,
+            ),
             ElevatedButton(
-              onPressed: () {
-                if (formkey.currentState!.validate()) {
-                  formkey.currentState!.save();
-                  addKategoriBarang();
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  await widget.saveChanges(widget.kategori_barang_model);
                   Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Add_Kategori_Barang()));
-                  var snackBar =
-                      const SnackBar(content: Text('Data Berhasil Disimpan'));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Kategori_Barang(),
+                    ),
+                  );
                 }
               },
-              child: const Text('Simpan'),
+              child: const Text('Save'),
             ),
           ],
         ),
