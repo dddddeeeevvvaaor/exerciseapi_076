11f85feea42deb1fbbdfde282c9698d4737fa989
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-05-13 18:45:00 +0700

    menambahkan variabel id pada model

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,46 +2,39 @@
 import 'dart:convert';
 
 class Kategori_Barang_Model {
-  final String nama;
+  final int id;
+  String nama;
   Kategori_Barang_Model({
-    required this.nama,
+    this.id = 0,
+    this.nama = '',
   });
-
-  Kategori_Barang_Model copyWith({
-    String? nama,
-  }) {
-    return Kategori_Barang_Model(
-      nama: nama ?? this.nama,
-    );
-  }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id_kategori_barang': id,
       'nama_kategori_barang': nama,
     };
   }
 
   factory Kategori_Barang_Model.fromMap(Map<String, dynamic> map) {
     return Kategori_Barang_Model(
-      nama: map['nama_kategori_barang'] as String,
+      id: map['id_kategori_barang'],
+      nama: map['nama_kategori_barang'],
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Kategori_Barang_Model.fromJson(String source) => Kategori_Barang_Model.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory Kategori_Barang_Model.fromJson(String source) =>
+      Kategori_Barang_Model.fromMap(json.decode(source));
 
-  @override
-  String toString() => 'Kategori_Barang_Model(nama: $nama)';
-
-  @override
-  bool operator ==(covariant Kategori_Barang_Model other) {
-    if (identical(this, other)) return true;
-  
-    return 
-      other.nama == nama;
+  Kategori_Barang_Model copyWith({
+    int? id,
+    String? nama,
+  }) {
+    return Kategori_Barang_Model(
+      id: id ?? this.id,
+      nama: nama ?? this.nama,
+    );
   }
-
-  @override
-  int get hashCode => nama.hashCode;
 }
